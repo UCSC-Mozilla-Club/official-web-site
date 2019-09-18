9157854e874ae25240045d19f441e72ce07cd005
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-09-18 07:28:11 +0530

    going to render team from a json

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,21 @@
-var changeContent = async (page) => {
+var changeContent = async (file) => {
     var loader = document.getElementById('loader');
     var loaderDiv = document.getElementById('loaderDiv');
     loaderDiv.hidden = false;
     loader.style = "width: 30%";
-    var page = await fetch(page);
+    var page = await fetch(file);
     loader.style = "width: 50%";
     if (page.status == 200) {
         document.getElementById("root").innerHTML = await page.text();
+        if (file === 'team.html')
+            loadTeam();
     } else {
         document.getElementById("root").innerHTML = "Something went wrong.";
     }
     loader.style = "width: 100%";
     loaderDiv.hidden = true;
 }
+
 
 $(document).ready(function () {
     $("root").on("load", changeContent('home.html'));
@@ -29,4 +32,16 @@ var loadSidebar = async () => {
     } else {
         document.getElementById("rightSidebar").innerHTML = "Something went wrong.";
     }
+}
+
+var loadTeam = async () => {
+    var teamResponse = await fetch("data/team.json");
+    var team = JSON.parse(await teamResponse.text());
+    var view = "";
+    team.forEach((element) => view += element['name']);
+    document.getElementById("team").innerHTML = view;
+}
+
+var teamCard = (member) => {
+    var card = "<div>" + member + "</div>";
 }
